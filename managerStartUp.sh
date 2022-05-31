bca69b021947832e3638ef4854d6c6bfe6848c8c
Karl Olson <76186248+KarlOlson@users.noreply.github.com>
2022-05-31 15:45:00 -0600

    Update managerStartUp.sh

--- managerStartUp.sh
+++ managerStartUp.sh
@@ -12,8 +12,8 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-
-curl -L "https://github.com/docker/compose/releases/download/$(curl https://github.com/docker/compose/releases | grep -m1 '<a href="/docker/compose/releases/download/' | grep -o 'v[0-9:].[0-9].[0-9]')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo apt -y install docker-compose
+#curl -L "https://github.com/docker/compose/releases/download/$(curl https://github.com/docker/compose/releases | grep -m1 '<a href="/docker/compose/releases/download/' | grep -o 'v[0-9:].[0-9].[0-9]')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
@@ -28,17 +28,4 @@ do
 done
 # Create docker registry
 sudo docker service create --name registry --publish published=5000,target=5000 registry:2
-# Start up services
-filename='containers/priority.txt'
-while read p; do
-    sudo docker-compose -f "containers/${p}/docker-compose.yml" build
-    sudo docker-compose -f "containers/${p}/docker-compose.yml" push
-    sudo docker service create \
-       --name "${p}-wrapper" \
-       --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
-       --mount type=bind,source="/local/repository/containers/${p}",target=/tmp/ \
-       --workdir /tmp/ \
-       docker/compose \
-       docker-compose up
-    echo "Deployed $p to swarm"
-done < "$filename"
+done
